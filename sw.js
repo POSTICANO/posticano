7010b3da01f52397aa75be41afd8b6416198c62d
POSTICANO <posticano@zohomail.in>
2026-05-22 12:46:47 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "freelance-pwa-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/starter.html",
+  "/business.html",
+  "/custom.html",
+  "/offer.html",
+  "/style.css",
+  "/script.js",
+  "/logo.svg",
+  "/favicon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
